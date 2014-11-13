e163c63f01be008e1e8cdb9932dfce964b16b282
Shinobu Aoki <emalock3@gmail.com>
2014-11-13 12:01:09 +0900

    initial commit

---
+++ src/main/java/testwebapp/App.java
@@ -0,0 +1,34 @@
+package testwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
+    @RestController
+    static class TestController {
+        @RequestMapping("/hello")
+        public String hello(@RequestParam String name) {
+            return String.format("Hello, %s!", name);
+        }
+    }
+}
